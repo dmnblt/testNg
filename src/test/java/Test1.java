cf5758ffda4edf079af2371dba8b81fa22e9d8bb
Duman <baltabayduman@mail.ru>
2023-05-19 15:31:17 +0600

    added final project files

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,18 +1,39 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.time.Duration;
+
 public class Test1 {
     private WebDriver driver;
-    private static final String BASE_URL = "https://www.linkedin.com";
+    private static final String BASE_URL = "https://acmp.ru/";
+    private static final String TASK_URL = "https://acmp.ru/index.asp?main=task&id_task=1234";
+    private static final String USERNAME  = "duman_";
+    private static final String PASSWORD = "87053570703d";
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(BASE_URL);
+        WebElement usernameInput = driver.findElement(By.name("lgn"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        usernameInput.sendKeys(USERNAME);
+        passwordInput.sendKeys(PASSWORD);
+        WebElement loginButton = driver.findElement(By.cssSelector("input[type='submit'][value='Ok']"));
+        loginButton.click();
+        WebElement lang = driver.findElement(By.linkText("[English]"));
+        lang.click();
     }
 
     @AfterMethod
@@ -20,112 +41,64 @@ public class Test1 {
         driver.quit();
     }
 
+//    @Test
+//    public void testRegistration() {
+//        driver.get(BASE_URL);
+//        WebElement registrationButton = driver.findElement(By.linkText("[регистрация]"));
+//        registrationButton.click();
+//        Assert.assertNotEquals(BASE_URL, driver.getCurrentUrl());
+//    }
+
     @Test
-    public void loginToLinkedIn() {
-        driver.get(BASE_URL);
-
-        WebElement signInButton = driver.findElement(By.linkText("Sign in"));
-        signInButton.click();
-        WebElement usernameInput = driver.findElement(By.id("username"));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        usernameInput.sendKeys("dumanb");
-        passwordInput.sendKeys("{password}");
-
-        WebElement signInButton2 = driver.findElement(By.xpath("//button[@type='submit']"));
-        signInButton2.click();
-        WebElement profileDropdown = driver.findElement(By.id("profile-nav-item"));
-        Assert.assertTrue(profileDropdown.isDisplayed(), "Login failed. Profile dropdown not found.");
+    public void testLoginWithValidCredentials() {
+        Assert.assertEquals(BASE_URL, driver.getCurrentUrl());
     }
 
     @Test
-    public void searchOnLinkedIn() {
+    public void testProblemSearchByValidId() {
         driver.get(BASE_URL);
-        WebElement searchInput = driver.findElement(By.name("keywords"));
-        searchInput.sendKeys("example search term");
-        WebElement searchDropdown = driver.findElement(By.id("search-dropdown-select"));
-        searchDropdown.click();
-        WebElement peopleOption = driver.findElement(By.xpath("//option[@value='PEOPLE']"));
-        peopleOption.click();
-        WebElement searchButton = driver.findElement(By.xpath("//button[@aria-label='Search']"));
+
+        WebElement searchInput = driver.findElement(By.name("str"));
+        searchInput.sendKeys("Edinitsy");
+
+        WebElement searchButton = driver.findElement(By.cssSelector("input[type='submit'][value='Ok']"));
         searchButton.click();
 
-        WebElement searchResults = driver.findElement(By.id("search-results-container"));
-        Assert.assertTrue(searchResults.isDisplayed(), "Search results not found.");
+        WebElement problemTitle = driver.findElement(By.cssSelector(".problem-name a"));
+        String actualProblemId = problemTitle.getText().trim();
+        Assert.assertEquals(actualProblemId, "Edinitsy", "Search results do not display the correct problem based on the ID.");
     }
 
     @Test
-    public void searchCustomerOnLinkedIn() {
+    public void testAccessProblemStatement() {
         driver.get(BASE_URL);
 
-        WebElement searchInput = driver.findElement(By.name("keywords"));
-        searchInput.sendKeys("John Doe");
+        driver.navigate().to(TASK_URL);
 
-        WebElement searchDropdown = driver.findElement(By.cssSelector("button.search-global-typeahead__button"));
-        searchDropdown.click();
-        WebElement peopleOption = driver.findElement(By.xpath("//button[text()='People']"));
-        peopleOption.click();
-
-        WebElement searchButton = driver.findElement(By.cssSelector("button.search-global-typeahead__button"));
-        searchButton.click();
-
-        WebElement searchResults = driver.findElement(By.cssSelector("div.search-results__list"));
-        Assert.assertTrue(searchResults.isDisplayed(), "Search results not found.");
+        WebElement problemStatement = driver.findElement(By.cssSelector(".problem-text"));
+        String statementText = problemStatement.getText();
+        Assert.assertTrue(statementText.contains("Problem statement"), "Problem statement is not displayed correctly.");
     }
 
     @Test
-    public void connectWithUserOnLinkedIn() {
+    public void testSolutionSubmission() {
         driver.get(BASE_URL);
-        WebElement searchInput = driver.findElement(By.name("keywords"));
-        searchInput.sendKeys("John Doe");
-        WebElement searchButton = driver.findElement(By.cssSelector("button.search-typeahead-v2__button"));
-        searchButton.click();
-        WebElement userProfileLink = driver.findElement(By.xpath("//span[contains(text(), 'John Doe')]"));
-        userProfileLink.click();
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        usernameInput.sendKeys(USERNAME);
+        passwordInput.sendKeys(PASSWORD);
+        WebElement loginButton = driver.findElement(By.cssSelector("input[type='submit'][value='Log in']"));
+        loginButton.click();
 
-        WebElement connectButton = driver.findElement(By.xpath("//button[contains(text(), 'Connect')]"));
-        connectButton.click();
+        driver.navigate().to(TASK_URL);
 
-        WebElement connectionTypeDropdown = driver.findElement(By.cssSelector("li.mn-connect-button__dropdown-item"));
-        connectionTypeDropdown.click();
-        WebElement personalNoteInput = driver.findElement(By.cssSelector("textarea.mn-connect-button__personalize-text"));
-        personalNoteInput.sendKeys("Hello, I would like to connect with you.");
+        WebElement solutionInput = driver.findElement(By.cssSelector(".sub a"));
+        solutionInput.sendKeys("<>");
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][value='Submit']"));
+        submitButton.click();
 
-        WebElement sendButton = driver.findElement(By.xpath("//button[contains(text(), 'Send')]"));
-        sendButton.click();
-
-        WebElement sentConfirmationMessage = driver.findElement(By.xpath("//span[contains(text(), 'Invitation Sent')]"));
-        Assert.assertTrue(sentConfirmationMessage.isDisplayed(), "Connection request failed.");
+        WebElement submissionFeedback = driver.findElement(By.cssSelector(".p-user-outbox p"));
+        String feedbackText = submissionFeedback.getText();
+        Assert.assertTrue(feedbackText.contains("Submission successful"), "Submission was not successful or feedback is incorrect.");
     }
-
-    @Test
-    public void editLinkedInProfile() {
-        driver.get(BASE_URL);
-
-        WebElement profilePicture = driver.findElement(By.cssSelector("button.nav-item__profile-member-photo.nav-item__icon"));
-        profilePicture.click();
-
-        WebElement viewProfileOption = driver.findElement(By.xpath("//span[text()='View profile']"));
-        viewProfileOption.click();
-
-        WebElement editButton = driver.findElement(By.xpath("//button[text()='Edit']"));
-        editButton.click();
-
-        WebElement headlineInput = driver.findElement(By.cssSelector("input#headline"));
-        headlineInput.clear();
-        headlineInput.sendKeys("Experienced Software Engineer");
-
-        WebElement summaryInput = driver.findElement(By.cssSelector("textarea#summary"));
-        summaryInput.clear();
-        summaryInput.sendKeys("Passionate about developing innovative solutions.");
-
-        WebElement saveButton = driver.findElement(By.xpath("//button[contains(text(), 'Save')]"));
-        saveButton.click();
-
-        WebElement successMessage = driver.findElement(By.xpath("//div[contains(text(), 'Profile updated')]"));
-        Assert.assertTrue(successMessage.isDisplayed(), "Profile update failed.");
-    }
-
-
-
-
 }
